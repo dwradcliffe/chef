28c0fda7de418d17c1cad6da163a9829ab77877e
Claire McQuin <claire@getchef.com>
2014-11-20 16:34:33 -0800

    Add audit to chef dsl. Write less code.

--- lib/chef/dsl.rb
+++ lib/chef/dsl.rb
@@ -1,6 +1,14 @@
-require 'chef/dsl/recipe'
-require 'chef/dsl/platform_introspection'
-require 'chef/dsl/data_query'
-require 'chef/dsl/include_recipe'
-require 'chef/dsl/include_attribute'
-require 'chef/dsl/registry_helper'
+
+dsl_files = [
+  'audit',
+  'data_query',
+  'include_attribute',
+  'include_recipe',
+  'platform_introspection',
+  'recipe',
+  'registry_helper'
+]
+
+dsl_files.each do |dsl_file|
+  require "chef/dsl/#{dsl_file}"
+end
